9a073dba5a6b424cc36a4f1bf9adca666c63dbec
leebaronx3 <leebaronx3@gmail.com>
2021-06-26 23:25:10 +0300

    added some css to user menu

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -12,18 +12,18 @@ import { useHistory } from 'react-router-dom';
 export default function UserMenu() {
     const history = useHistory();
     return (
-        <>
+        <div className='user-menu-wrapper'>
+            <p>Signed in as
+                <span className='d-block font-weight-bold'>leebaronx3</span>
+            </p>
+            <hr className='border-dark' />
             <Nav /*defaultActiveKey="/home"*/ className="flex-column">
-                <p>Signed in as<br />
-                    leebaronx3
-                </p>
-                <hr />
                 <Nav.Link onClick={() => history.push('/usersproject')}>My Projects</Nav.Link>
                 <Nav.Link onClick={() => history.push('/settings')}>Settings</Nav.Link>
                 <Nav.Link>Log Out</Nav.Link>
 
             </Nav>
 
-        </>
+        </div>
     )
 }
