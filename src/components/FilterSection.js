aa5b4b63cb5d1d2e4ec677c86731f5e749f33d75
leebaronx3 <leebaronx3@gmail.com>
2021-06-27 23:56:11 +0300

    uninstalled react-multiselect-checkbox and installed react-multi-select-component instead, turned MultiTechSelect to a general multi select MyMultiSelect and replaced for diffiulty level and assets in FliterSection

--- src/components/FilterSection.js
+++ src/components/FilterSection.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Form, FormControl } from 'react-bootstrap'
 import { BiSearch } from 'react-icons/bi'
-import MultiTechSelect from './MultiTechSelect'
+import MyMultiSelect from './MyMultiSelect'
 
 export default function FilterSection() {
     return (
@@ -15,39 +15,17 @@ export default function FilterSection() {
             <Form>
                 <Form.Row>
                     <p className='mr-4 font-weight-bold'>Filter By</p>
-                    {/* <Form.Group controlId="requiredTechSelect" className='mr-2'>
-                        
-                        <Form.Control as="select">
-                            <option disabled hidden selected value=''>Required Technologies</option>
-                            <option>HTML</option>
-                            <option>CSS</option>
-                            <option>JavaScript</option>
-                        </Form.Control>
-                    </Form.Group> */}
-                    <MultiTechSelect />
-
-                    <Form.Group controlId="difficultyLvlSelect" className='ml-2 mr-2'>
-                        <Form.Control as="select">
-                            <option disabled hidden selected value='' >Difficulty Level</option>
-                            <option value='1'>Easy</option>
-                            <option value='2'>Medium</option>
-                            <option value='3'>Hard</option>
-                            <option value=''>All Levels</option>
-                        </Form.Control>
-                    </Form.Group>
-
-                    <Form.Group controlId="assetsSelect" className='ml-2'>
-                        <Form.Control as="select">
-                            <option disabled hidden selected value='' >Assets</option>
-                            <option value='1'>Included</option>
-                            <option value='0'>Not Included</option>
-                            <option value=''>Either</option>
-                        </Form.Control>
-                    </Form.Group>
+                    <div className='mr-2'>
+                        <MyMultiSelect type='tech' />
+                    </div>
+                    <div className='mr-2'>
+                        <MyMultiSelect type='difficultyLvls' />
+                    </div>
+                    <MyMultiSelect type='assets' />
                 </Form.Row>
             </Form>
             <Form>
-                <Form.Row>
+                <Form.Row className='sort-by-section'>
                     <p className='mr-4 font-weight-bold'>Sort By</p>
                     <Form.Check
                         custom
