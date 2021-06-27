d2a794871ddbcb525e7527aff52c409f48fe0482
leebaronx3 <leebaronx3@gmail.com>
2021-06-27 21:49:36 +0300

    added temp static notifications counter badge

--- src/components/MyPopover.js
+++ src/components/MyPopover.js
@@ -5,7 +5,9 @@ import { OverlayTrigger, Popover } from 'react-bootstrap'
 import { BiBell, BiUser } from 'react-icons/bi'
 import Notifications from './Notifications'
 import userProfileImagePlaceholder from '../images/usersImages/user_id_1/img-placeholder.png'
+import { useState } from 'react'
 export default function MyPopover({ type }) {
+    const [notifsCount, setNotifsCount] = useState(2)
     return (
         <>
             <OverlayTrigger
@@ -29,7 +31,10 @@ export default function MyPopover({ type }) {
                                 type === 'usermenu' ?
                                     <img src={userProfileImagePlaceholder} className='header-user-img rounded-circle' />
                                     :
-                                    <BiBell />
+                                    <div className='mt-n3'>
+                                        <BiBell />
+                                        {notifsCount > 0 ? <div className='notif-badge rounded-circle bg-white text-dark text-center mt-n4 font-weight-bold ml-3'>{2}</div> : null}
+                                    </div>
                         }
                     </button>
                 }
